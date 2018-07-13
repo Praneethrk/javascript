df4242fe8965edfa695e7b8e97271804a2901a0e
Praneethrk <praneethrk17@gmail.com>
2018-07-13 16:23:52 +0530

    commit 2

---
+++ js/main.js
@@ -0,0 +1,36 @@
+function isEven() {
+    var num = document.getElementById('num').value;
+    var message;
+    if(num%2 == 0){
+       message = "number"+num+" is even";
+    } 
+    else{
+       message = "number"+num+" is odd";
+    }
+    document.getElementById('r').innerHTML=message;
+}
+
+
+function bill(){
+    var units = document.getElementById('units').value;
+    var min_amount = 50;
+    var amount = 0;
+    var total_amount = 0;
+    
+    if(units>=0&&units<100)
+        {
+            amount=units * 5;
+        }else if(units>=100&&units<500)
+        {
+            amount=units * 6;
+        }else
+        {
+            amount=units * 10;
+        }
+     
+    var total_amount = amount+min_amount;
+    /*msg='bill amount='+total_amount;*/
+    document.getElementById('bills').innerHTML=total_amount;
+    
+
+}
